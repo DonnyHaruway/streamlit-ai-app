829f6022ec1181debf5ca698e46ceb0cfe10d48a
DonnyHaruway <tai.smz55@gmail.com>
2025-02-13 11:16:04 +0900

    第2章のプログラムを実装しました

--- 02-3_qa_advance.py
+++ 02-3_qa_advance.py
@@ -1,10 +1,8 @@
-from dotenv import load_dotenv
 import streamlit as st
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
-load_dotenv()
 
 def main():
 
